1473d1a3ddc6c57bc17a8e544e788b562b0fb894
Peter Schuller <peter.schuller@infidyne.com>
2009-07-18 21:45:32 +0200

    * skeleton logging

---
+++ src/shastity/logging.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+Thin wrapper around the standard logging module. The remainder of
+shastity should never refer directly to the standard logging module.
+"""
+
+from __future__ import absolute_import
+from __future__ import with_statement
+
+import logging
+
+# Mirror log levels, and add the missing (why!??!?!) NOTICE.
+DEBUG = logging.DEBUG
+INFO = logging.INFO
+NOTICE = (logging.WARNING + logging.INFO) / 2
+WARNING = logging.WARNING
+ERROR = logging.ERROR
+CRITICAL = logging.CRITICAL
+
+# make sure there was room between INFO and WARNING
+assert NOTICE > logging.INFO
+assert NOTICE < logging.WARNING
+
+# and make sure we have not misunderstood the numerical interpretation
+# of levels
+assert INFO < WARNING
+
+def get_logger(name):
+    """
+    Our version of logging.getLogger().
+    """
+    return logging.getLogger(name)
+
---
+++ test/test_logging.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import with_statement
+
+import unittest
+
+import shastity.logging as logging
+
+class LoggingTests(unittest.TestCase):
+    # not much to test quite yet
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
