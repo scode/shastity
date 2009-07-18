eca416e634d916ceaf9eee1f2a419b91493005f3
Peter Schuller <peter.schuller@infidyne.com>
2009-07-18 13:43:02 +0200

    * initial hierarchy

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name="shastity",
+      version="0.0",
+      author="Peter Schuller",
+      author_email="peter.schuller@infidyne.com",
+      packages = ['shastity'],
+      package_dir = {'': 'src'},
+      scripts = ['bin/shastity'])
+
+
