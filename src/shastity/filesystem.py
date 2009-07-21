44110c697c3628c063893bfee70d2c2e0f3d524f
Peter Schuller <peter.schuller@infidyne.com>
2009-07-21 21:14:29 +0200

    * another, hopefully final, copyright date tweak

--- src/shastity/filesystem.py
+++ src/shastity/filesystem.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2008 Peter Schuller <peter.schuller@infidyne.com>
+# Copyright (c) 2009 Peter Schuller <peter.schuller@infidyne.com>
 
 """
 Provide a thin wrapper around file system operations. Reasons for
