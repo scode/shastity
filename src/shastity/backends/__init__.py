26a260cacd928a05bb171599f6c26b7ff79c1d19
Peter Schuller <peter.schuller@infidyne.com>
2009-07-25 00:41:08 +0200

    * add trivial memory-backed backend

---
+++ src/shastity/backends/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2009 Peter Schuller <peter.schuller@infidyne.com>
---
+++ src/shastity/backends/memorybackend.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2009 Peter Schuller <peter.schuller@infidyne.com>
+
+'''
+In-memory backend.
+'''
+
+import shastity.backend as backend
+
+class MemoryBackend(backend.Backend):
+    '''Trivial in-memory backend that simply maps all operations to an
+    internal dict. Obviously this does violate the supposed
+    persistence guarantee of a backend; this is intended for unit
+    testing purposes.'''
+    def __init__(self, identifier):
+        backend.Backend.__init__(self, identifier)
+
+        self.__dict = dict()
+
+    def put(self, name, data):
+        self.__dict[name] = data
+
+    def get(self, name):
+        return self.__dict[name]
+
+    def list(self):
+        return self.__dict.keys()
+
+    def delete(self, name):
+        del(self.__dict[name])
+
+    def close(self):
+        pass
+    
