86d38e3d95ae4fdae3fc61c1151d588edb053adc
Peter Schuller <peter.schuller@infidyne.com>
2009-07-25 17:17:44 +0200

    * preliminary stab at meta data representation

--- src/shastity/filesystem.py
+++ src/shastity/filesystem.py
@@ -34,6 +34,166 @@ import os.path
 import shutil
 import tempfile
 
+class FileMetaData(object):
+    '''Represents meta-data about files, including any and all
+    meta-data that are to be preserved on backup/restore.
+    
+    We make a deliberate effort to provide a high-level abstraction
+    rather than POSIX style bit twiddling. We essentially provide a
+    dedicated accessor for every relevant piece of information, along
+    with methodsfor converting to/from a text format readable both by
+    humans (sort of) and ourselves.
+
+    Instances of this class only care about meta data; they know
+    nothing of the files to which the meta data appliies, or
+    underlying file system operations (construction of FileMetaData
+    instances from some actual real file is done by the appropriate
+    file system backend).
+
+    Instances of this class are to be treated as read-only, and the
+    public interface deliberately makes it difficult to make changes.
+
+    @ivar is_directory        Is the file a directory?
+    @ivar is_character_device Is the file a special character device?
+    @ivar is_block_device     Is the file a block device?
+    @ivar is_regular          Is the file a regularfile?
+    @ivar is_fifo             Is the file a FIFO/named pipe?
+    @ivar is_symlink          Is the file a symbolic link?
+    @ivar uid                 UID of the owner.
+    @ivar gid                 GID of the group owner.
+    @ivar size                Size of file (if regular, else None).
+    @ivar atime               Access time of the file (seconds since epoch).
+    @ivar mtime               Modification time of the file (seconds since epoch).
+    @ivar ctime               ctime, whatever the platform feels that means (secondssince epoch).
+    @ivar is_setuid           Is the setuid bit set?
+    @ivar is_setgid           Is the setgid bit set?
+    @ivar is_sticky           Is the sticky bit set?
+    @ivar yser_read
+    @ivar group_read
+    @ivar other_read
+    @ivar user_write
+    @ivar group_write
+    @ivar other_write
+    @ivar user_execute
+    @ivar group_execute
+    @ivar other_execute
+    '''
+    def __init__(self, props=None, other=None):
+        '''
+        @param props: Dict of properties that match those of the instance to be created.
+        @param other: Other instance on which to base the values of any properties that
+                      do not appear in props.
+        '''
+        pass
+
+    @classmethod
+    def from_string(cls, s):
+        '''Given a string in the format produced by to_string(), parse
+        it and return the resulting instance.'''
+        raise NotImplementedError
+
+    def to_string(self):
+        '''Produce a string encoding of this meta data.
+
+        TODO: define format characteristics'''
+        raise NotImplementedError
+
+    @property
+    def is_directory(self):
+        return self.__is_directory
+
+    @property
+    def is_character_device(self):
+        return self.__is_character_device
+
+    @property
+    def is_block_device(self):
+        return self.__is_block_device
+
+    @property
+    def is_regular(self):
+        return self.__is_regular
+
+    @property
+    def is_fifo(self):
+        return self.__is_fifo
+
+    @property
+    def is_symlink(self):
+        return self.__is_symlink
+
+    @property
+    def uid(self):
+        return self.__uid
+
+    @property
+    def gid(self):
+        return self.__gid
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def atime(self):
+        return self.__atime
+
+    @property
+    def mtime(self):
+        return self.__mtime
+
+    @property
+    def ctime(self):
+        return self.__ctime
+
+    @property
+    def is_setuid(self):
+        return self.__is_setuid
+
+    @property
+    def is_setgid(self):
+        return self.__is_setgid
+    
+    @property
+    def is_sticky(self):
+        return self.__is_sticky
+
+    @property
+    def user_read(self):
+        return self.__user_read
+
+    @property
+    def group_read(self):
+        return self.__user_read
+
+    @property
+    def other_read(self):
+        return self.__other_read
+
+    @property
+    def user_write(self):
+        return self.__user_write
+    
+    @property
+    def group_write(self):
+        return self.__group_write
+
+    @property
+    def other_write(self):
+        return self.__other_write
+
+    @property
+    def user_execute(self):
+        return self.__user_execute
+
+    @property
+    def group_execute(self):
+        return self.__group_execute
+    
+    @property
+    def other_execute(self):
+        return self.__other_execute
+
 class StaleTemporaryDirectory(Exception):
     '''Raised to indicate that an attempt to use a stale (cleaned up)
     temporary directory was detected.'''
