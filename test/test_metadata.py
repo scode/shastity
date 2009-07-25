79ca968b3f0eb497079559a4a19f32abc1aeaa8f
Peter Schuller <peter.schuller@infidyne.com>
2009-07-25 19:11:09 +0200

    * test write protection

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -9,14 +9,18 @@ import errno
 import os.path
 import unittest
 
-import shastity.metadata as md
+import shastity.metadata as metadata
 
 class MetaDataTests(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_misc(self):
-        pass
+    def test_write_protection(self):
+        md = metadata.FileMetaData()
+        
+        def assign_test():
+            md.user_write = False
+        self.assertRaises(AssertionError, assign_test)
 
 if __name__ == "__main__":
     unittest.main()
