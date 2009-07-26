fa28ada80a907a80f63e0630de7c942556d87661
Peter Schuller <peter.schuller@infidyne.com>
2009-07-26 21:19:02 +0200

    * provide a 'mock-up' logger for use in testing

--- src/shastity/logging.py
+++ src/shastity/logging.py
@@ -34,6 +34,42 @@ def get_logger(name):
     """
     return logging.getLogger(name)
 
+class FakeLogger(object):
+    '''Fake logger mostly intended for unit testing. For its context
+    managed life time it will patch away a given attribute with
+    itself, mocking away the logging functions.'''
+    def __init__(self, module, attrname):
+        self.mocked_module = module
+        self.mocked_attr = attrname
+        self.real_log = getattr(module, attrname)
+
+    def __enter__(self):
+        setattr(self.mocked_module, self.mocked_attr, self)
+
+    def __exit__(self, *args, **kwargs):
+        setattr(self.mocked_module, self.mocked_attr, self.real_log)
+
+    def log(self, *args, **kwargs):
+        pass
+
+    def critical(self, *args, **kwargs):
+        pass
+
+    def error(self, *args, **kwargs):
+        pass
+
+    def warning(self, *args, **kwargs):
+        pass
+
+    def notice(self, *args, **kwargs):
+        pass
+
+    def info(self, *args, **kwargs):
+        pass
+
+    def debug(self, *args, **kwargs):
+        pass
+
 # TODO: this is bad (to do this on module import).
 logging.basicConfig()
 
