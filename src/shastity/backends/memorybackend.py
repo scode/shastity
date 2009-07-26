8a49a99dc5f25538377d0cc971e2aa8ab425fbca
Peter Schuller <peter.schuller@infidyne.com>
2009-07-26 20:38:08 +0200

    * fix memory backend which wasn't implementing the concurrency   semantics required of backends

--- src/shastity/backends/memorybackend.py
+++ src/shastity/backends/memorybackend.py
@@ -6,10 +6,17 @@
 In-memory backend.
 '''
 
+from __future__ import absolute_import
+from __future__ import with_statement
+
 import random
+import threading
 import time
 
 import shastity.backend as backend
+
+_dict = dict()
+_lock = threading.Lock()
 
 class MemoryBackend(backend.Backend):
     '''Trivial in-memory backend that simply maps all operations to an
@@ -21,12 +28,15 @@ class MemoryBackend(backend.Backend):
     of seconds between 0 and which each operation will spend time im
     time.sleep(). This is intended, again, for unit testing purposes
     in order to try to trigger various timing dependent cases in a
-    non-deterministic fashion.'''
+    non-deterministic fashion.
+
+    In order to simulate external storage that is shared between
+    instances, it keeps thread-safe access to an instance independent
+    shared dict for storage.'''
     def __init__(self, identifier, opts=dict()):
         backend.Backend.__init__(self, identifier, opts)
 
         self.__max_fake_delay = float(opts.get('max_fake_delay', 0.0))
-        self.__dict = dict()
 
     def __delay(self):
         if self.__max_fake_delay > 0.0:
@@ -41,22 +51,34 @@ class MemoryBackend(backend.Backend):
     def put(self, name, data):
         self.__delay()
 
-        self.__dict[name] = data
+        global _dict
+        global _lock
+        with _lock:
+            _dict[name] = data
 
     def get(self, name):
         self.__delay()
 
-        return self.__dict[name]
+        global _dict
+        global _lock
+        with _lock:
+            return _dict[name]
 
     def list(self):
         self.__delay()
 
-        return self.__dict.keys()
+        global _dict
+        global _lock
+        with _lock:
+            return _dict.keys()
 
     def delete(self, name):
         self.__delay()
 
-        del(self.__dict[name])
+        global _dict
+        global _lock
+        with _lock:
+            del(_dict[name])
 
     def close(self):
         self.__delay()
