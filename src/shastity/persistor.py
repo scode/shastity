109050a15e2525768eba826ed57f851698603be8
Peter Schuller <peter.schuller@infidyne.com>
2009-07-27 23:39:55 +0200

    * skeleton persistor module

---
+++ src/shastity/persistor.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2009 Peter Schuller <peter.schuller@infidyne.com>
+
+'''
+Takes stream(s) of (path, metadata) and persists to backends.
+'''
+
+from __future__ import absolute_import
+from __future__ import with_statement
+
+import  os.path
+
+import shastity.filesystem as filesystem
+import shastity.logging as logging
+
+log = logging.get_logger(__name__)
+
+def persist_full(fs, traversal, storagequeue):
+    '''
+    '''
+    pass
