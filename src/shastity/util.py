d8ef03a7005ec4b83b97c89c55d582f1b7d80bab
Peter Schuller <peter.schuller@infidyne.com>
2009-12-16 23:15:28 +0100

    * provide bind() utility function for re-binding variables for   lambdas

--- src/shastity/util.py
+++ src/shastity/util.py
@@ -6,3 +6,13 @@
 Misc utilities.
 '''
 
+def bind(callable, *bind_args, **bind_kwargs):
+    def caller(*args, **kwargs):
+        actual_args = bind_args + args
+
+        actual_kwargs = bind_kwargs
+        actual_kwargs.update(kwargs)
+
+        return callable(*actual_args, **actual_kwargs)
+
+    return caller
