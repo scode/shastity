8844210399cd7848e7f55287bf9d056600817556
Peter Schuller <peter.schuller@infidyne.com>
2009-12-17 00:25:18 +0100

    * baby step towards cmdline parsing

--- src/shastity/cmdline.py
+++ src/shastity/cmdline.py
@@ -13,14 +13,39 @@ shastity.operations module.
 from __future__ import absolute_import
 from __future__ import with_statement
 
+import logging
 import optparse
+import sys
 
-def build_parser():
+class CommandLineError(Exception):
+    """
+    Raised to indicate a problem with the command line.
+    """
+    pass
+
+def _build_parser():
     parser = optparse.OptionParser()
 
     return parser
 
-def main():
-    option_parser = build_parser()
+def _interpret_cmdline(options, args):
+    raise CommandLineError('not implemented')
 
-    pass
+def main():
+    try:
+        option_parser = _build_parser()
+
+        options, args = option_parser.parse_args()
+
+        operation, args, kwargs, config = _interpret_cmdline(options, args)
+
+        getattr(operations, operation)(*(args + (config,)))
+
+        sys.exit(0)
+    except CommandLineError, e:
+        logging.error('shastity: command line error: %s', unicode(e))
+    except Exception, e:
+        logging.exception('shastity: error: %s', unicode(e))
+
+    sys.exit(1) # todo: communiate more detail to callers
+
