e12bb6311286afa5bacff0a56cf19630acfcd238
Peter Schuller <peter.schuller@infidyne.com>
2011-02-12 22:27:12 +0100

    Hello clojure.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject shastity "0.1-SNAPSHOT"
+  :description "De-duplicating encrypting remote backup tool"
+  :url "https://github.com/scode/shastity"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
