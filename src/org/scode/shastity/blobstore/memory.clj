f8a076dc329341d0c574e8b2a639a4968aa4ef90
Peter Schuller <peter.schuller@infidyne.com>
2011-02-18 00:03:06 +0100

    Minimal memory store impl.

--- src/org/scode/shastity/blobstore/memory.clj
+++ src/org/scode/shastity/blobstore/memory.clj
@@ -1,10 +1,11 @@
 (ns org.scode.shastity.blobstore.memory
   (:require [org.scode.shastity.blobstore :as blobstore]))
 
-(deftype MemoryStore []
+(deftype MemoryStore [contents]
   blobstore/BlobStore
-  (put-blob [store name vaule] nil)
-  (get-blob [store name] nil)
-  (delete-blob [store name] nil)
-  (list-blobs [store] nil))
+  (put-blob [store name value] (dosync (commute contents conj [name value])))
+  (get-blob [store name] (get @contents name))
+  (delete-blob [store name] (dosync (commute contents dissoc name)))
+  (list-blobs [store] (keys @contents)))
+
 
