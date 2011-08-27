75fbca5503ea43fa7b81261d34f8d1bbe10e53c7
Peter Schuller <peter.schuller@infidyne.com>
2011-08-27 15:35:18 +0200

    Add some assertions.

--- test/org/scode/shastity/java/test.clj
+++ test/org/scode/shastity/java/test.clj
@@ -3,4 +3,7 @@
   (:use [clojure.test]))
 
 (deftest byte-empty-equals
-  (is (= Bytes/EMPTY (Bytes.))))
+  (is (= Bytes/EMPTY (Bytes.)))
+  (is (= Bytes/EMPTY Bytes/EMPTY))
+  (is (= (Bytes.) (Bytes.)))
+  (is (= (Bytes.) (Bytes. (byte-array 0)))))
