0df3b3ea8b12d533d865f931f0a3330ddc2f7005
Peter Schuller <peter.schuller@infidyne.com>
2011-08-27 16:50:40 +0200

    Add various utf8/hex convenience.

--- java-src/org/scode/shastity/java/Bytes.java
+++ java-src/org/scode/shastity/java/Bytes.java
@@ -1,5 +1,6 @@
 package org.scode.shastity.java;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 /**
@@ -9,6 +10,9 @@ import java.util.Arrays;
  */
 public class Bytes {
     public static final Bytes EMPTY = new Bytes();
+
+    private static final char[] hexAlphas = new char[]
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     private byte[] array;
 
@@ -55,6 +59,99 @@ public class Bytes {
 
     public byte[] getMutableByteArray() {
         return this.array;
+    }
+
+    /**
+     * Construct a Bytes instance representing the given String encoded in UTF-8.
+     *
+     * @param str The string.
+     * @throws RuntimeException if an UnsupportedEncodingException is thrown.
+     *
+     * @return The Bytes instance.
+     */
+    public static Bytes fromString(String str) {
+        // Rely on String.getBytes() returning something we own.
+        try {
+            return Bytes.wrapArray(str.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param ch 0-9a-f
+     * @throws IllegalArgumentException if not 0-9a-f
+     * @return The int
+     */
+    private static int hexCharToValue(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return (ch - '0');
+        } else if (ch >= 'a' && ch <= 'f') {
+            return (ch - 'a');
+        } else {
+            throw new IllegalArgumentException("hex char must be 0-9 or a-f");
+        }
+    }
+
+    /**
+     * Return a Bytes instance containing the bytes reprecented hexadecimally (lower case alphas) in the
+     * given string.
+     *
+     * @param hex The hex string.
+     *
+     * @throws IllegalArgumentException if the hexadeciaml string cannot be parsed
+     *
+     * @return The Bytes instance.
+     */
+    public static Bytes fromHex(String hex) {
+        if (hex.length() % 2 != 0) {
+            throw new IllegalArgumentException("hexadecimal strings must be of even length");
+        }
+
+        byte[] arr = new byte[hex.length() / 2];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (byte)((hexCharToValue(hex.charAt(i * 2)) << 4)
+                    | (hexCharToValue(hex.charAt(i * 2 + 1))));
+        }
+
+        return Bytes.wrapArray(arr);
+    }
+
+    /**
+     * Return a hexadecimal string representing this Bytes.
+     *
+     * @return The hexadeciaml string.
+     */
+    public String toHex() {
+        StringBuffer sb = new StringBuffer();
+
+        for (int b : this.array) {
+            sb.append(hexAlphas[b >> 4]);
+            sb.append(hexAlphas[b & 0x0F]);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * @throws RuntimeException if an UnsupportedEncodingException is thrown
+     *
+     * @return The string.
+     */
+    public String toStringFromUtf8() {
+        try {
+            return new String(this.array, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String toString() {
+        return "<" + this.toHex() + ">";
+    }
+
+    public int length() {
+        return this.array.length;
     }
 
     @Override
--- test/org/scode/shastity/java/test.clj
+++ test/org/scode/shastity/java/test.clj
@@ -7,3 +7,20 @@
   (is (= Bytes/EMPTY Bytes/EMPTY))
   (is (= (Bytes.) (Bytes.)))
   (is (= (Bytes.) (Bytes. (byte-array 0)))))
+
+(deftest byte-empty-utf8
+  (is (= "" (.toStringFromUtf8 Bytes/EMPTY)))
+  (is (= "" (.toStringFromUtf8 (Bytes/fromString "")))))
+
+(deftest byte-wrap
+  (let [b (byte-array 1)]
+    (is (= b (.getMutableByteArray (Bytes/wrapArray b))))))
+
+(deftest byte-utf8
+  (is (= "åäö" (.toStringFromUtf8 (Bytes/fromString "åäö")))))
+
+(deftest byte-hex
+  (is (= "74657374" (.toHex (Bytes/fromString "test"))))
+  (is (= 1 (.length (Bytes/fromHex "76"))))
+  (is (= "test" (.toStringFromUtf8 (Bytes/fromHex "74657374"))))
+  (is (= "test" (.toStringFromUtf8 (Bytes/fromHex (.toHex (Bytes/fromString "test")))))))
