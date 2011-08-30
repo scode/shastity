420335666bf21d6bdca6247cf8e25e074809c04c
Peter Schuller <peter.schuller@infidyne.com>
2011-08-30 23:42:21 +0200

    Use clojure 1.2.1.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject shastity "0.1-SNAPSHOT"
   :description "De-duplicating encrypting remote backup tool"
   :url "https://github.com/scode/shastity"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.amazonaws/aws-java-sdk "1.2.7"]]
   :java-source-path "java-src"
