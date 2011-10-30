cfe46a426011a566fb49b82061b63ebdd8bb128f
Peter Schuller <peter.schuller@infidyne.com>
2011-10-30 19:53:31 +0100

    Test input->...->output identitiy of manifests.

--- test/org/scode/shastity/manifest/test.clj
+++ test/org/scode/shastity/manifest/test.clj
@@ -32,13 +32,33 @@
                             ["name" {:key 1} [ "daedbeef" "beefdead" ]]]]
     (is (= [name meta hashes] (manifest/decode-object (manifest/encode-object [name meta hashes]))))))
 
-(deftest empty-manifest
-  (with-store [store]
-    (let [writer (manifest/create-manifest-writer)]
-      (manifest/freeze writer)
-      (manifest/upload writer store "empty-manifest"))
-    (let [reader (manifest/create-manifest-reader)]
-      (is (= (seq []) (manifest/get-objects reader store "empty-manifest"))))))
+(defmacro defmanifest-identity [name manifest-entries]
+  "Define a test with the given name suffix and the given manifest entries, which will
+  create a temporary store and write a manifest to it containing the given files (in the order
+  given). Then, read manifest back and compare to make sure the returned sequence of entries
+  is identical to the *sorted* original sequence."
+  `(deftest ~(symbol (str "manifest-identity-" name))
+     (with-store [store#]
+       (let [entries# ~manifest-entries]
+         (let [writer# (manifest/create-manifest-writer)]
+           (doseq [[path# meta# hashes#] entries#]
+             (manifest/add-object writer# path# meta# hashes#))
+           (manifest/freeze writer#)
+           (manifest/upload writer# store# "manifest"))
+         (let [reader# (manifest/create-manifest-reader)]
+           (is (= (seq (sort manifest/manifest-entry-comparator entries#))
+                  (seq (manifest/get-objects reader# store# "manifest")))))))))
+
+(defmanifest-identity empty [])
+(defmanifest-identity one-entry [["name" {:key "value"} ["deadbeef"]]])
+(defmanifest-identity one-entry-nohashes [["name" {:key "value"} []]])
+(defmanifest-identity one-entry-multiple-hashes [["name" {:key "value"} ["deadbeef" "beefdead"]]])
+(defmanifest-identity multiple-entries [["a" {:key 1} ["dead"]]
+                                        ["b" {:key 2} ["beef"]]
+                                        ["c" {:key 3} ["00ba"]]])
+(defmanifest-identity multiple-entries-unordered [["b" {:key 2} ["beef"]]
+                                                  ["a" {:key 1} ["dead"]]
+                                                  ["c" {:key 3} ["00ba"]]])
 
 
 
