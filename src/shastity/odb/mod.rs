10773632109bc722e0a6d6d89069dfeb499c322a
Peter Schuller <peter.schuller@infidyne.com>
2016-01-31 10:11:44 -0800

    Remove use of inappropriate lifetime.
    It got added because I was exploring how to implement cause()
    correctly and confused myself. After sneaking a peak at some of the
    Cargo code, I arrived the current implementation.

--- src/shastity/odb/mod.rs
+++ src/shastity/odb/mod.rs
@@ -3,28 +3,29 @@ use std::error::Error;
 use std::vec::Vec;
 
 #[derive(Debug)]
-pub struct OdbError<'a> {
-    cause: Box<Error + 'a>
+pub struct OdbError {
+    cause: Option<Box<Error>>
 }
-
 
 pub trait Odb {
     fn put(value: &[u8]) -> Box<Error>;
     fn get(value: &[u8]) -> Result<Vec<u8>, Box<Error>>;
 }
 
-impl<'a> Error for OdbError<'a> {
+impl Error for OdbError {
     fn description(&self) -> &str {
         // TODO: be useful
         "Generic OdbError"
     }
 
     fn cause(&self) -> Option<&Error> {
-        Some(self.cause.as_ref())
+        self.cause.as_ref().map(|c| {
+            &**c
+        })
     }
 }
 
-impl<'a> fmt::Display for OdbError<'a> {
+impl fmt::Display for OdbError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         try!(write!(f, "OdbError: {}", self.description()));
         match self.cause() {
