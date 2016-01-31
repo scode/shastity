4e41ba311f46f7aed77a64a8e98b16f30311bb50
Peter Schuller <peter.schuller@infidyne.com>
2016-01-30 23:52:41 -0800

    Early beginnings of Odb.

---
+++ src/shastity/odb/mod.rs
@@ -0,0 +1,40 @@
+use std::fmt;
+use std::error::Error;
+use std::vec::Vec;
+
+#[derive(Debug)]
+pub struct OdbError<'a> {
+    cause: Box<Error + 'a>
+}
+
+
+pub trait Odb {
+    fn put(value: &[u8]) -> Box<Error>;
+    fn get(value: &[u8]) -> Result<Vec<u8>, Box<Error>>;
+}
+
+impl<'a> Error for OdbError<'a> {
+    fn description(&self) -> &str {
+        // TODO: be useful
+        "Generic OdbError"
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        Some(self.cause.as_ref())
+    }
+}
+
+impl<'a> fmt::Display for OdbError<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: de-duplicate first write
+        match self.cause() {
+            None => {
+                write!(f, "OdbError: {}", self.description())
+            }
+            Some(cause) => {
+                try!(write!(f, "OdbError: {}", self.description()));
+                fmt::Display::fmt(cause, f)
+            }
+        }
+    }
+}
