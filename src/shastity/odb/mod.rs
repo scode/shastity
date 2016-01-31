21ff14f17c7d93b88f210dd1f1db30ccf9d52233
Peter Schuller <peter.schuller@infidyne.com>
2016-01-30 23:59:42 -0800

    Avoid success temporary by returning Ok(()), and fix lack of try!().

--- src/shastity/odb/mod.rs
+++ src/shastity/odb/mod.rs
@@ -26,10 +26,10 @@ impl<'a> Error for OdbError<'a> {
 
 impl<'a> fmt::Display for OdbError<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let success = write!(f, "OdbError: {}", self.description());
+        try!(write!(f, "OdbError: {}", self.description()));
         match self.cause() {
             None => {
-                success
+                Ok(())
             }
             Some(cause) => {
                 fmt::Display::fmt(cause, f)
