de83ab608e3ca290bf61096657e0f446cb2bf245
Peter Schuller <peter.schuller@infidyne.com>
2016-02-01 22:34:57 -0800

    Odb::put() now returns key.

--- src/shastity/odb/mod.rs
+++ src/shastity/odb/mod.rs
@@ -8,7 +8,7 @@ pub struct OdbError {
 }
 
 pub trait Odb {
-    fn put(value: &[u8]) -> Result<(), Box<Error>>;
+    fn put(value: &[u8]) -> Result<Vec<u8>, Box<Error>>;
     fn get(key: &[u8]) -> Result<Vec<u8>, Box<Error>>;
 }
 
