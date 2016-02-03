efddc824a33f288c3a27335aa72cf3078c2d8e17
Peter Schuller <peter.schuller@infidyne.com>
2016-02-02 20:44:48 -0800

    Minimal documentation of Odb.

--- src/shastity/odb/mod.rs
+++ src/shastity/odb/mod.rs
@@ -7,6 +7,35 @@ pub struct OdbError {
     cause: Option<Box<Error>>
 }
 
+/// A content addressable object database.
+///
+/// For information on the general concept of content addressable
+/// storage, see:
+///
+///   https://en.wikipedia.org/wiki/Content-addressable_storage
+///
+/// Properties of an Odb include:
+///
+///   - If two objects have the same key, they are identical. Thus,
+///     two objects can be compared for equality without pulling the
+///     content out of the store.
+///   - Once an object is recorded in the store, it does not go away
+///     unless explicit removal if requested through some means beyond
+///     the scope of this trait. In other words, puts are durable.
+///     For example, a loacl file system store would probably need to
+///     fsync() prior to returning. (This does not preclude e.g.
+///     in memory implementation.)
+///   - Keys can never be predicted by the caller. Only keys returned
+///     from put() can be assumed to be valid.
+///
+/// Caveats:
+///
+///   - Batch based puts would allow for greater performance w/o
+///     concurrency.
+///   - No attempt is made to support very large objects (which would
+///     complicate the interface). Chunking is recommended to be
+///     performed on top.
+///   - There is no provision for concurrent access at this time.
 pub trait Odb {
     fn put(value: &[u8]) -> Result<Vec<u8>, Box<Error>>;
     fn get(key: &[u8]) -> Result<Vec<u8>, Box<Error>>;
