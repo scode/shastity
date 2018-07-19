3c243321b62874bf2854fae0a4f1e9b9dc944139
Peter Schuller <peter.schuller@infidyne.com>
2018-07-18 23:38:53 -0700

    Improve panic strings.

--- tests/weakstore/mod.rs
+++ tests/weakstore/mod.rs
@@ -4,20 +4,20 @@ pub fn test_weak_get(store: &mut kv::WeakStore) {
     match store.weak_get("test".as_bytes()) {
         Ok(Some(_)) => panic!("store should have been empty"),
         Ok(None) => (),
-        Err(e) => panic!("get should not have failed: {}", e),
+        Err(e) => panic!("weak_get should have succeeded: {}", e),
     };
 }
 
 pub fn test_weak_put(store: &mut kv::WeakStore) {
     match store.weak_put("k".as_bytes(), "v".as_bytes()) {
         Ok(()) => (),
-        Err(e) => panic!("put should have succeeded: {}", e),
+        Err(e) => panic!("weak_put should have succeeded: {}", e),
     }
 
     match store.weak_get("test".as_bytes()) {
         Ok(Some(_)) => panic!("store should have been empty"),
         Ok(None) => (),
-        Err(e) => panic!("get should not have failed: {}", e),
+        Err(e) => panic!("weak_get should have succeeded: {}", e),
     };
 }
 
@@ -25,31 +25,31 @@ pub fn test_weak_exists(store: &mut kv::WeakStore) {
     match store.weak_exists("k".as_bytes()) {
         Ok(true) => panic!("key should not have existed"),
         Ok(false) => (),
-        Err(e) => panic!("weak_exists should not have failed: {}", e),
+        Err(e) => panic!("weak_exists should have succeeded: {}", e),
     }
 
     match store.weak_put("k".as_bytes(), "v".as_bytes()) {
         Ok(()) => (),
-        Err(e) => panic!("put should have succeeded: {}", e),
+        Err(e) => panic!("weak_put should have succeeded: {}", e),
     }
 
     match store.weak_exists("k".as_bytes()) {
         Ok(true) => (),
         Ok(false) => panic!("key should have existed"),
-        Err(e) => panic!("weak_exists should not have failed: {}", e),
+        Err(e) => panic!("weak_exists should have succeeded: {}", e),
     }
 }
 
 pub fn test_weak_delete(store: &mut kv::WeakStore) {
     match store.weak_put("k".as_bytes(), "v".as_bytes()) {
         Ok(()) => (),
-        Err(e) => panic!("put should have succeeded: {}", e),
+        Err(e) => panic!("weak_put should have succeeded: {}", e),
     }
 
     match store.weak_exists("k".as_bytes()) {
         Ok(true) => (),
         Ok(false) => panic!("key should have existed"),
-        Err(e) => panic!("weak_exists should not have failed: {}", e),
+        Err(e) => panic!("weak_exists should have succeeded: {}", e),
     }
 
     match store.weak_delete("k".as_bytes()) {
