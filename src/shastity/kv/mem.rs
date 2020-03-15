e66b0d68f4de7496366d3b680ad2bd1948c4dc07
Peter Schuller <peter.schuller@infidyne.com>
2020-03-15 00:35:42 -0700

    Edition 2018 (for real).

--- src/shastity/kv/mem.rs
+++ src/shastity/kv/mem.rs
@@ -1,4 +1,3 @@
-use kv;
 use std::collections::HashMap;
 
 /// A simple in-memory hash map based store for purposes of testing.
@@ -15,30 +14,30 @@ impl MemWeakStore {
     }
 }
 
-impl<'a> kv::WeakStore<'a> for MemWeakStore {
-    fn weak_get(&mut self, key: &[u8]) -> Result<Option<Vec<u8>>, kv::StoreError> {
+impl<'a> super::WeakStore<'a> for MemWeakStore {
+    fn weak_get(&mut self, key: &[u8]) -> Result<Option<Vec<u8>>, super::StoreError> {
         Ok(self.map.get(key).map(|v| v.to_vec()))
     }
 
-    fn weak_put(&mut self, key: &[u8], value: &[u8]) -> Result<(), kv::StoreError> {
+    fn weak_put(&mut self, key: &[u8], value: &[u8]) -> Result<(), super::StoreError> {
         self.map.insert(key.to_owned(), value.to_owned());
         Ok(())
     }
 
-    fn weak_exists(&mut self, key: &[u8]) -> Result<bool, kv::StoreError> {
+    fn weak_exists(&mut self, key: &[u8]) -> Result<bool, super::StoreError> {
         Ok(self.map.contains_key(key))
     }
 
-    fn weak_delete(&mut self, key: &[u8]) -> Result<(), kv::StoreError> {
+    fn weak_delete(&mut self, key: &[u8]) -> Result<(), super::StoreError> {
         self.map.remove(key);
         Ok(())
     }
 
     fn weak_iter(
         &mut self,
-        _cursor: Option<kv::Cursor>,
+        _cursor: Option<super::Cursor>,
         _max_items: usize,
-    ) -> Result<kv::IterationResult, kv::StoreError> {
+    ) -> Result<super::IterationResult, super::StoreError> {
         unimplemented!()
     }
 }
